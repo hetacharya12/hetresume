d9d50ecda2414c3bf52b43b9a93bc94026f3412e
hetacharya12 <hetacharya@gmail.com>
2018-08-29 10:47:38 -0400

    Added months calculator

--- Jobs/views.py
+++ Jobs/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from .models import Job, Myimage
+from datetime import datetime
 
+def diff_month(d1, d2):
+    return (d1.year - d2.year) * 12 + d1.month - d2.month
 
 def home(request):
     jobs = Job.objects
     images = Myimage.objects
+    no_of_months=diff_month(datetime.now(), datetime(2014,7,7))+20
     for imageurls in images.all():
         myimageurl = imageurls.image.url
-    return render(request, 'jobs/index-5.html',{'jobs': jobs})
+    return render(request, 'jobs/index-5.html',{'jobs': jobs,'no_of_months':no_of_months})
